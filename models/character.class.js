2156486c48cd8e8909c098d56d147560a9b47c13
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-01-18 20:15:17 +0100

    movable character

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-
+    speed = 8;
     height = 300;
     width = 150;
     x = 10;
@@ -13,21 +13,23 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-26.png'
     ]
     currentImage = 0;
-
+    world;
 
     constructor() {
         super().loadImage('../img/2_character_pepe/1_idle/idle/I-1.png')
         this.loadImages(this.IMAGES_WALKING)
-
         this.animate()
     }
 
     animate() {
         setInterval(() => {
+            if (this.world.keyboard.RIGHT){
             let i = this.currentImage % this.IMAGES_WALKING.length;
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
+            this.x += this.speed;
+        }
         }, 100);
     }
 
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,9 +2,9 @@ class Cloud extends MovableObject {
   y = 20;
   height = 250;
   width = 500;
-  speed =0.2;
+  speed = 0.2;
 
-  
+
   constructor(path) {
     super().loadImage(path)
     this.x = Math.random() * 500;
